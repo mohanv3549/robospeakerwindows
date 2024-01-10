48c603b342d7f9a88f29ba4e2b154d2bf53c845b
mohanv3549 <143386839+mohanv3549@users.noreply.github.com>
2024-01-10 14:05:57 +0530

    Add files via upload

---
+++ robospeaker.py
@@ -0,0 +1,13 @@
+import os
+if __name__ == '__main__':
+    print("WELCOME TO ROBO SPEAKER.............")
+    print("VER 1.2v")
+    while True:
+        s=input("what do you want me to speak:")
+        if(s=='0'):
+            os.system('powershell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{bye bye}\')"')
+            break
+        else:
+            command=f'powershell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{s}\')"'
+            os.system(command)
+
